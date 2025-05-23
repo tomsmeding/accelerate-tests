1e9bca6c79542930342ec7bdb01e88624fca1232
Tom Smeding <tom@tomsmeding.com>
2025-05-23 19:36:12 +0200

    NondetSumhang: Add note about expected time

--- src/Data/Array/Accelerate/Tests/Prog/NondetSumhang.hs
+++ src/Data/Array/Accelerate/Tests/Prog/NondetSumhang.hs
@@ -8,7 +8,7 @@ import Data.Array.Accelerate.Tests.Types
 test :: Test
 test = mkTestOn "nondet-sumhang" "Hangs with accelerate-llvm#97 on jizo" [PTX] $ \runN -> do
   let arr = A.fromList (Z :. 257 :. 31) [1::Int ..]
-  putStrLn "running:"
+  putStrLn "running (this should complete in <1sec, otherwise it's hanging):"
   print $ length $ show $ runN (A.scanl1 (+)) arr
   putStrLn "done."
   return True
